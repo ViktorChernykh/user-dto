b62264e286835f7d89dd2a9191efd78f538a4c2e
Viсtor Chernykh <v-cher9@yandex.ru>
2022-08-13 21:30:53 +0300

    Add RefreshTokenRequest

---
+++ Sources/UserDto/Request/RefreshTokenRequest.swift
@@ -0,0 +1,16 @@
+//
+//  RefreshTokenRequest.swift
+//  Addswift
+//
+//  Created by Victor Chernykh on 13.08.2022.
+//
+
+public struct RefreshTokenRequest: Codable {
+    // MARK: - Stored properties
+    public let token: String
+    
+    // MARK: - Init
+    public init(token: String) {
+        self.token = token
+    }
+}
