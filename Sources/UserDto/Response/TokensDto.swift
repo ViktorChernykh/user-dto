a50d3c316eb1b3a0f6a2cc3150cec1b18f04d553
ViktorChernykh <v-cher9@yandex.ru>
2022-11-14 19:10:25 +0300

    Add axpiresAt to TokensDto

--- Sources/UserDto/Response/TokensDto.swift
+++ Sources/UserDto/Response/TokensDto.swift
@@ -10,14 +10,17 @@ import Foundation
 public struct TokensDto: Codable {
     // MARK: - Stored properties
     public let access: String
+    public let expiresAt: Date
     public let refresh: String
     
     // MARK: - Init
     public init(
         access: String,
+        expiresAt: Date,
         refresh: String
     ) {
         self.access = access
+        self.expiresAt = expiresAt
         self.refresh = refresh
     }
 }
