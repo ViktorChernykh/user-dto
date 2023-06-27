8c6fd22e66b2dfe5bc9dce7c0f7daabffdf1c688
ViktorChernykh <v-cher9@yandex.ru>
2023-06-28 00:13:05 +0300

    Change AdminDto

--- Sources/UserDto/Response/AdminDto.swift
+++ Sources/UserDto/Response/AdminDto.swift
@@ -12,6 +12,9 @@ public struct AdminDto: Codable {
     // MARK: - Stored properties
     public let id: UUID?
     public let email: String
+	public let isAdmin: Bool
+	public let isBlocked: Bool
+	public let isEmailConfirmed: Bool
     public let firstName: String
     public let lastName: String?
     public let role: String
@@ -21,6 +24,9 @@ public struct AdminDto: Codable {
     public init(
         id: UUID? = nil,
         email: String,
+		isAdmin: Bool,
+		isBlocked: Bool,
+		isEmailConfirmed: Bool,
         firstName: String,
         lastName: String?,
 		role: String,
@@ -28,6 +34,9 @@ public struct AdminDto: Codable {
     ) {
         self.id = id
         self.email = email
+		self.isAdmin = isAdmin
+		self.isBlocked = isBlocked
+		self.isEmailConfirmed = isEmailConfirmed
         self.firstName = firstName
         self.lastName = lastName
         self.role = role
